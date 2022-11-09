39bacdd4185c5049d2564b106167033544ea346c
Alvin Kiveu <67068458+alvin-kiveu@users.noreply.github.com>
2022-11-09 15:01:32 +0300

    addding all data

---
+++ umsportal cli/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_cl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO SOCIAL CLI");
+            Console.WriteLine("Please login");
+            Console.Write("Username : ");
+            string username = Console.ReadLine();
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Loading....");
+            if(username == "admin" || password == "admin")
+            {
+                Console.WriteLine("logged in successful ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Wellcome SOCIAL CLI");
+                Console.WriteLine("Username : " + username);
+                Console.WriteLine("Password : " + username);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("😐 Loging failed !!!!");
+            }
+            //REQUESTINGI FOR LOGING
+            Console.ReadKey();
+        }
+    }
+}
